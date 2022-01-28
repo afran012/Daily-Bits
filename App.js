63f1bfb8670759573c9fa813a3a89e4cad02b1c2
Jader Gomez <91078172+jaderfg@users.noreply.github.com>
2022-01-28 12:45:57 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Menu from "../components/Menu";
+import Principal from "../components/Principal";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Principal />
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
